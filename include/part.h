cea9d518804025a0670e8e8be5177285393f671b
Vyacheslav Bocharov <devel@lexina.in>
2021-05-25 12:48:17 +0300

    Fix spaces-tab

--- include/part.h
+++ include/part.h
@@ -29,7 +29,7 @@ struct block_drvr {
 #define PART_TYPE_ISO		0x03
 #define PART_TYPE_AMIGA		0x04
 #define PART_TYPE_EFI		0x05
-#define PART_TYPE_AML       0x06
+#define PART_TYPE_AML		0x06
 /* maximum number of partition entries supported by search */
 #define DOS_ENTRY_NUMBERS	8
 #define ISO_ENTRY_NUMBERS	64
